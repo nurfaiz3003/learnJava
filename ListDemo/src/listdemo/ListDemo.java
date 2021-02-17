4e4f44ae03df278666e61be82d6e91eb998e761c
Nurfaiz Herlambang <nurfaiz3003@gmail.com>
2021-02-17 14:13:14 +0700

    List Demo

---
+++ ListDemo/src/listdemo/ListDemo.java
@@ -0,0 +1,33 @@
+package listdemo;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class ListDemo {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> userAgeList = new LinkedList<>();
+
+        userAgeList.add(40);
+        userAgeList.add(53);
+        userAgeList.add(45);
+        userAgeList.add(53);
+        userAgeList.add(2, 51);
+
+        System.out.println(userAgeList.size());
+        userAgeList.remove(3);
+        System.out.println(userAgeList.contains(12));
+        System.out.println(userAgeList.indexOf(12));
+        System.out.println(userAgeList.get(2));
+
+        Integer[] userAgeArray = userAgeList.toArray(new Integer[0]);
+        System.out.println(userAgeArray[0]);
+        System.out.println(userAgeList);
+        System.out.println(userAgeList.peek());
+        System.out.println(userAgeList);
+        System.out.println(userAgeList.poll());
+        System.out.println(userAgeList);
+        System.out.println(userAgeList.pollLast());
+        System.out.println(userAgeList);
+    }
+}
